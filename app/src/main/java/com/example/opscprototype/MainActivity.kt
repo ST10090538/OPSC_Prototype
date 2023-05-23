8f393cca3d771ccc682738cee935778057a29ff9
TIGGI010 <124901532+TIGGI010@users.noreply.github.com>
2023-05-23 13:41:39 +0200

    pie

--- app/src/main/java/com/example/opscprototype/MainActivity.kt
+++ app/src/main/java/com/example/opscprototype/MainActivity.kt
@@ -1,3 +1,5 @@
+
+
 package com.example.opscprototype
 
 import android.content.Intent
--- app/src/main/java/com/example/opscprototype/PersonalProgressPage.kt
+++ app/src/main/java/com/example/opscprototype/PersonalProgressPage.kt
@@ -1,14 +1,40 @@
 package com.example.opscprototype
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 
 class PersonalProgressPage : AppCompatActivity() {
+    lateinit var pieChart: PieChart
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.progress_personalprogress_page)
+
+        //------------
+
+
+        pieChart = findViewById(R.id.pie_chart)
+
+        val entries = listOf(
+            PieEntry(50f, "Web Design"),
+            PieEntry(30f, "App Development"),
+
+        )
+
+        val dataSet = PieDataSet(entries, "Pie Chart")
+        val pieData = PieData(dataSet)
+
+        pieChart.data = pieData
+        pieChart.invalidate()
+
+    //--------------
 
         val progressIcon = findViewById<ImageView>(R.id.personalprogress_progress_icon)
         val profileIcon = findViewById<ImageView>(R.id.personalprogress_profile_icon)
@@ -25,5 +51,7 @@ class PersonalProgressPage : AppCompatActivity() {
         timesheetIcon.setOnClickListener {
             startActivity(Intent(this, TimesheetPage::class.java))
         }
+
+
     }
 }
