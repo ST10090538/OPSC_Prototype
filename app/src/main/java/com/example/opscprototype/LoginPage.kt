cc666bafc14b1bc79000073f28d41e2c3b8e5a22
ShaleenKalan <126862314+ShaleenKalan@users.noreply.github.com>
2023-05-23 13:28:08 +0200

    login and register pages

--- app/src/main/java/com/example/opscprototype/LoginPage.kt
+++ app/src/main/java/com/example/opscprototype/LoginPage.kt
@@ -2,23 +2,60 @@ package com.example.opscprototype
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginPage : AppCompatActivity() {
+
+    private lateinit var editText2: EditText
+    private lateinit var editTextTextPassword: EditText
+    private lateinit var loginpage_login_button: Button
+    private lateinit var registerpage_register_button: Button
+
+    private var registeredUsername: String? = null
+    private var registeredPassword: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
 
-        val login = findViewById<Button>(R.id.loginpage_login_button)
-        val register = findViewById<Button>(R.id.registerpage_register_button)
+            editText2 = findViewById(R.id.editText2)
+            editTextTextPassword = findViewById(R.id.editTextTextPassword)
+            loginpage_login_button = findViewById(R.id.loginpage_login_button)
+            registerpage_register_button = findViewById(R.id.registerpage_register_button)
 
-        login.setOnClickListener {
-            startActivity(Intent(this, TimesheetPage::class.java))
-        }
+            registerpage_register_button.setOnClickListener {
+                startActivity(Intent(this, RegisterPage::class.java))
+            }
 
-        register.setOnClickListener {
-            startActivity(Intent(this, RegisterPage::class.java))
+            val extras = intent.extras
+            if (extras != null) {
+                registeredUsername = extras.getString("username")
+                registeredPassword = extras.getString("password")
+            }
+
+            loginpage_login_button.setOnClickListener {
+                val username = editText2.text.toString().trim()
+                val password = editTextTextPassword.text.toString().trim()
+
+                if (TextUtils.isEmpty(username)) {
+                    editText2.error = "Username is Required!"
+                    return@setOnClickListener
+                } else if (TextUtils.isEmpty(password)) {
+                    editTextTextPassword.error = "Password is Required!"
+                    return@setOnClickListener
+                } else if (username != registeredUsername || password != registeredPassword) {
+                    Toast.makeText(this, "Invalid username or password!", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                } else {
+                    Toast.makeText(this, "Logged In Successfully!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, TimesheetPage::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
         }
     }
-}
