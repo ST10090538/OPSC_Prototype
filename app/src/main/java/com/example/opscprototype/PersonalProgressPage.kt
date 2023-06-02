15906f89c882fb6c2d47b780fe8c458fc3bf7566
SBezuidenhout25 <st10084961@vcconnect.edu.za>
2023-06-02 19:01:56 +0200

    Added tasks to the personal progress page.

--- app/src/main/java/com/example/opscprototype/PersonalProgressPage.kt
+++ app/src/main/java/com/example/opscprototype/PersonalProgressPage.kt
@@ -1,6 +1,4 @@
 package com.example.opscprototype
-
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
@@ -10,21 +8,24 @@ import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.ColorTemplate
 import java.time.Duration
 
 
-class PersonalProgressPage : AppCompatActivity() {
+class PersonalProgressPage : AppCompatActivity(), OnChartValueSelectedListener {
     private lateinit var pieChart: PieChart
+    private var displayTasks = false
 
     @RequiresApi(Build.VERSION_CODES.S)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.progress_personalprogress_page)
         pieChart = findViewById(R.id.pie_chart)
@@ -36,7 +37,7 @@ class PersonalProgressPage : AppCompatActivity() {
         val entries = mutableListOf<PieEntry>()
         var totalHoursWorked = Duration.ZERO
 
-        for(cat in SharedData.lstCategories) {
+        for (cat in SharedData.lstCategories) {
             totalHoursWorked += cat.hoursWorked
         }
 
@@ -60,6 +61,8 @@ class PersonalProgressPage : AppCompatActivity() {
         pieChart.highlightValues(null)
         pieChart.invalidate()
 
+        pieChart.setOnChartValueSelectedListener(this)
+
         progressIcon.setOnClickListener {
             startActivity(Intent(this, ProgressPage::class.java))
         }
@@ -71,8 +74,8 @@ class PersonalProgressPage : AppCompatActivity() {
         timesheetIcon.setOnClickListener {
             startActivity(Intent(this, TimesheetPage::class.java))
         }
-
     }
+
     private fun getColors(categoryCount: Int): List<Int> {
         val colors = mutableListOf<Int>()
         val colorTemplate = ColorTemplate.MATERIAL_COLORS
@@ -82,5 +85,86 @@ class PersonalProgressPage : AppCompatActivity() {
         }
 
         return colors
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        if (!displayTasks) {
+            if (e != null && e is PieEntry) {
+                val selectedCategory = e.label
+                val tasksInCategory = getTasksInCategory(selectedCategory)
+                updatePieChart(tasksInCategory)
+                displayTasks = true
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onNothingSelected() {
+        if (displayTasks) {
+            val categories = SharedData.lstCategories
+            val entries = mutableListOf<PieEntry>()
+            var totalHoursWorked = Duration.ZERO
+
+            for (cat in SharedData.lstCategories) {
+                totalHoursWorked += cat.hoursWorked
+            }
+
+            for (cats in SharedData.lstCategories) {
+                val hoursWorked = cats.hoursWorked.toSeconds().toFloat()
+                val percentage = (hoursWorked / totalHoursWorked.toSeconds().toFloat()) * 100
+                entries.add(PieEntry(percentage, cats.strName))
+            }
+
+            val dataSet = PieDataSet(entries, "")
+            dataSet.sliceSpace = 3f
+            dataSet.selectionShift = 5f
+            dataSet.colors = getColors(SharedData.lstCategories.size)
+
+            val data = PieData(dataSet)
+            data.setValueFormatter(PercentFormatter(pieChart))
+            data.setValueTextSize(11f)
+            data.setValueTextColor(Color.BLACK)
+
+            pieChart.data = data
+            pieChart.highlightValues(null)
+            pieChart.invalidate()
+
+            displayTasks = false
+        }
+    }
+
+    private fun getTasksInCategory(category: String): List<Tasks> {
+        return SharedData.lstTasks.filter { it.strCategory == category }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun updatePieChart(tasks: List<Tasks>) {
+        val entries = mutableListOf<PieEntry>()
+        var totalHoursWorked = Duration.ZERO
+
+        for (task in tasks) {
+            totalHoursWorked += task.durTimeWorked
+        }
+
+        for (task in tasks) {
+            val hoursWorked = task.durTimeWorked.toSeconds().toFloat()
+            val percentage = (hoursWorked / totalHoursWorked.toSeconds().toFloat()) * 100
+            entries.add(PieEntry(percentage, task.strTaskName))
+        }
+
+        val dataSet = PieDataSet(entries, "")
+        dataSet.sliceSpace = 3f
+        dataSet.selectionShift = 5f
+        dataSet.colors = getColors(tasks.size)
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter(pieChart))
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.data = data
+        pieChart.highlightValues(null)
+        pieChart.invalidate()
     }
 }
