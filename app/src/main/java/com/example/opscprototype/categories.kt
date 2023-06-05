67f5b47860b54dafba2f290fbd37524ab65b4eec
TIGGI010 <124901532+TIGGI010@users.noreply.github.com>
2023-06-05 15:54:39 +0200

    Color Change

--- app/src/main/java/com/example/opscprototype/categories.kt
+++ app/src/main/java/com/example/opscprototype/categories.kt
@@ -5,7 +5,19 @@ import androidx.annotation.RequiresApi
 import java.time.Duration
 
 class categories (strCatName: String){
+    private val categories: MutableList<categories> = mutableListOf()
     @RequiresApi(Build.VERSION_CODES.O)
     var hoursWorked = Duration.ZERO
     val strName = strCatName
+    fun addCategory(category: categories) {
+        categories.add(category)
+    }
+
+    fun getCategories(): List<categories> {
+        return categories.toList()
+    }
+
+
+
+
 }
