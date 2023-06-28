94cce5e0a1cc5ed296d70e98089269a5ad7ecdd6
SBezuidenhout25 <st10084961@vcconnect.edu.za>
2023-06-28 17:22:22 +0200

    Registration and Login fixed

--- app/src/main/java/com/example/opscprototype/RegisterPage.kt
+++ app/src/main/java/com/example/opscprototype/RegisterPage.kt
@@ -2,95 +2,89 @@ package com.example.opscprototype
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterPage : AppCompatActivity() {
 
-
-  private lateinit var editText2: EditText
+    private lateinit var editText2: EditText
     private lateinit var editTextTextPassword: EditText
     private lateinit var editTextTextPassword2: EditText
     private lateinit var registerpage_register_button: Button
-
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_page)
 
-        // spinner implemented for user to select province
-        var provinceSpinner = findViewById<Spinner>(R.id.provinceSpinner)
-        var provinces = arrayOf(
-            "Eastern Cape",
-            "Free State",
-            "Gauteng",
-            "KwaZulu-Natal",
-            "Limpopo",
-            "Mpumalanga",
-            "North West",
-            "Northern Cape",
-            "Western Cape"
-        )
-        var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, provinces)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        provinceSpinner.adapter = adapter
+        // Initialize Firebase Authentication
+        firebaseAuth = FirebaseAuth.getInstance()
 
-
-       editText2 = findViewById(R.id.editText2)
+        editText2 = findViewById(R.id.editText2)
         editTextTextPassword = findViewById(R.id.editTextTextPassword)
         editTextTextPassword2 = findViewById(R.id.editTextTextPassword2)
         registerpage_register_button = findViewById(R.id.registerpage_register_button)
 
         registerpage_register_button.setOnClickListener {
-          val username = editText2.text.toString().trim()
+            val username = editText2.text.toString().trim()
             val password = editTextTextPassword.text.toString().trim()
             val confpass = editTextTextPassword2.text.toString().trim()
 
-            //validation for empty username field
-            if (username.isEmpty()){
+            // Validation for empty username field
+            if (TextUtils.isEmpty(username)) {
                 editText2.error = "Username is Required!"
                 return@setOnClickListener
-
             }
-            //validation for empty password field
-            if (password.isEmpty()) {
+
+            // Validation for empty password field
+            if (TextUtils.isEmpty(password)) {
                 editTextTextPassword.error = "Password is Required!"
                 return@setOnClickListener
             }
-            //validation for an eight-character password
-            var passwordPattern = Regex("^.{8,}$")  // Regex pattern for 8 or more characters
+
+            // Validation for an eight-character password
+            val passwordPattern = Regex("^.{8,}$")  // Regex pattern for 8 or more characters
             if (!password.matches(passwordPattern)) {
                 editTextTextPassword.error = "Password must be 8 characters or longer!"
                 return@setOnClickListener
             }
-            //validation for empty confirm password field
-            if (confpass.isEmpty()) {
+
+            // Validation for empty confirm password field
+            if (TextUtils.isEmpty(confpass)) {
                 editTextTextPassword2.error = "Confirm Password is Required!"
                 return@setOnClickListener
             }
-            // validation for passwords to match
+
+            // Validation if passwords match
             if (password != confpass) {
                 editTextTextPassword2.error = "Password does not match!"
-            }
-            // validation if passwords match
-            if (password == confpass) {
-
-                Toast.makeText(this, "Registered Successfully!", Toast.LENGTH_SHORT).show()
-
-                val intent = Intent(this, LoginPage::class.java)
-                intent.putExtra("username", username)
-                 intent.putExtra("password", password)
-                startActivity(intent)
-                finish()
-         } else {
-                editTextTextPassword2.error = "Password does not match!"
+                return@setOnClickListener
             }
 
+            // Register user with Firebase Authentication
+            firebaseAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // User registration successful
+                        Toast.makeText(this, "Registered Successfully!", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, LoginPage::class.java)
+                        intent.putExtra("username", username)
+                        intent.putExtra("password", password)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // User registration failed
+                        Toast.makeText(this, "Registration Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
